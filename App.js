3710e1bf8c851dc3ba1e7961d568666391b7e96c
Stanislaw Skalkovich <stas.skalkovich@gmail.com>
2022-03-31 21:11:55 +0300

    2

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Alert} from 'react-native';
+
+function handleClick() { 
+  Alert.alert(
+    'Важное сообщение системы',
+    "нажмите да или нет!",
+    [
+    {text: 'NO', onPress: () => alert("Попробуй еще раз")},
+    {text: 'OK', onPress: () => alert("Ты гей")}
+    ],
+    { cancelable: false },
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Привет никон</Text>
+      <Text nativeID='text' style={styles.ring} onPress={handleClick}></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +29,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  ring:{
+    width: 100,
+    height: 100,
+    borderColor: 'black',
+    borderRadius: 50,
+    borderWidth: 8,
+    borderStyle: "solid",
+    textAlign: 'center',
+    textAlignVertical: 'center'
+  }
 });
