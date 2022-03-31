614294339c4a53354d9a67c8ba9b452b440f46d2
NcUndstnd <95880613+NcUndstnd@users.noreply.github.com>
2022-03-31 23:04:22 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function nykon() {
+function nykon() {
   return(
       <Text style={{fontSize: 40 + 'px', color: 'purple', textAlign: 'center'}}>Я НИКОН ЧИГОЯ РАБОТАЮ 7 ДНЕЙ БЕЗ ВЫХОДНЫХ И ЗАРПЛАТЫ</Text>
   );
 }
 
 export default function App() {
-  nykon();
   return (
     <View style={styles.container}>
+      {nykon()}
       <Text>Хуй</Text>
       <StatusBar style="auto" />
     </View>
