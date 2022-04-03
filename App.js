5740037d98bee4f14ffe8901c5107f4d66b4ca89
Stanislaw Skalkovich <stas.skalkovich@gmail.com>
2022-04-03 23:53:35 +0300

    a

--- App.js
+++ App.js
@@ -5,10 +5,7 @@ import { ImageBackground } from 'react-native-web';
 export default function App() {
   return (
     <View style={styles.container}>
-     <Image
-      style={styles.image}
-      source={require("./files/splash1.png")}>
-      </Image>
+      <Text>Хуй</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,11 +18,5 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     width: '100%',
     height:'100%'
-  },
-  image:{
-    
-    width: 100,
-    height: 100
-    
   }
 });
